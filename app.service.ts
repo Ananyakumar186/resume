1a8ea53f962e732ed40b6cff8c7cee69fc76c861
Ananya Kumar <60437807+Ananyakumar186@users.noreply.github.com>
2023-09-26 13:25:34 +0530

    handset

--- app.service.ts
+++ app.service.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@angular/core';
 import { BreakpointObserver, BreakpointState, Breakpoints } from '@angular/cdk/layout';
+import { BehaviorSubject } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class AppService{
-
-  constructor(public breakpointObserver: BreakpointObserver) { }
-  isMobile(): boolean {
-    return this.breakpointObserver.isMatched(Breakpoints.HandsetPortrait) || this.breakpointObserver.isMatched(Breakpoints.XSmall);
+  isHandset =  new BehaviorSubject<boolean>(false);
+  constructor(public breakpointObserver: BreakpointObserver) {
+    breakpointObserver.observe(['(max-width:600px)']).subscribe(result=>{
+      if(result.matches){
+        this.isHandset.next(true);
+        console.log('ishandset');
+      }else{
+        this.isHandset.next(false);
+        console.log('not ishandset');
+      }
+    });
   }
+
 
 }
